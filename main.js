b6b854115907e2e0360c52c209b94cada11c150f
Akash Kumar <100992918+aakashhkumaar@users.noreply.github.com>
2024-08-07 10:47:49 +0530

    Update main.js
    changed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
   document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed(".text", {
-      strings: ["Frontend Developer", "YouTuber", "Web Developer"],
+      strings: ["Frontend Developer", "Software Developer", "Designer"],
       typeSpeed: 100,
       backSpeed: 100,
       backDelay: 1000,
